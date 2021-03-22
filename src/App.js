fd8a9322fa124c41546d90f6447638eb8172567e
rainasong <rainasong0509@gmail.com>
2021-03-23 00:55:44 +0800

    feat: added navbar

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import './App.css';
 import Homepage from "./components/homepage";
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import MeetingPage from "./components/meetingPage";
 import ReviewPage from "./components/reviewPage";
 import KeywordPage from "./components/keywordPage";
 import SummaryPage from "./components/summaryPage";
 import PieChartPage from "./components/pieChartPage";
+import Navbar from "./components/navbar/navbar";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
         <Router>
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/meeting">Start Meeting</Link>
-                        </li>
-                        <li>
-                            <Link to="/review">Review</Link>
-                        </li>
-                        <li>
-                            <Link to="/keyword">Keyword</Link>
-                        </li>
-                        <li>
-                            <Link to="/summary">Summary</Link>
-                        </li>
-                        <li>
-                            <Link to="/pieChart">Pie Chart</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+                <Navbar/>
                 <Switch>
                     <Route path="/" exact component={Homepage}/>
                     <Route path="/meeting" component={MeetingPage}/>
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './navbar.css';
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar">
+                <ul className="navbarContainer">
+                    <li className="navbarItem">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="navbarItem">
+                        <Link to="/meeting">Start Meeting</Link>
+                    </li>
+                    <li className="navbarItem">
+                        <Link to="/review">Review</Link>
+                    </li>
+                    <li className="navbarItem">
+                        <Link to="/keyword">Keyword</Link>
+                    </li>
+                    <li className="navbarItem">
+                        <Link to="/summary">Summary</Link>
+                    </li>
+                    <li className="navbarItem">
+                        <Link to="/pieChart">Pie Chart</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
