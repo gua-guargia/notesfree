7d512a9fb0e97a058e146aec52c71e29886f19da
rainasong <rainasong0509@gmail.com>
2021-03-23 00:38:47 +0800

    feat: basic page structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import Homepage from "./components/homepage";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import MeetingPage from "./components/meetingPage";
+import ReviewPage from "./components/reviewPage";
+import KeywordPage from "./components/keywordPage";
+import SummaryPage from "./components/summaryPage";
+import PieChartPage from "./components/pieChartPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/meeting">Start Meeting</Link>
+                        </li>
+                        <li>
+                            <Link to="/review">Review</Link>
+                        </li>
+                        <li>
+                            <Link to="/keyword">Keyword</Link>
+                        </li>
+                        <li>
+                            <Link to="/summary">Summary</Link>
+                        </li>
+                        <li>
+                            <Link to="/pieChart">Pie Chart</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/" exact component={Homepage}/>
+                    <Route path="/meeting" component={MeetingPage}/>
+                    <Route path="/review" component={ReviewPage}/>
+                    <Route path="/keyword" component={KeywordPage}/>
+                    <Route path="/summary" component={SummaryPage}/>
+                    <Route path="/pieChart" component={PieChartPage}/>
+                </Switch>
+            </div>
+        </Router>
     </div>
   );
 }
---
+++ src/components/pieChartPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PieChartPage = () => {
+    return (
+        <div>
+            <h1>pie chart page</h1>
+        </div>
+    );
+}
+
+export default PieChartPage;
